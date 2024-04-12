a6999a65ed8804d39b81b5f307adee4f074597f1
TKDKB <63513743+TKDKB@users.noreply.github.com>
2024-04-12 15:45:19 +0300

    current

--- run.sh
+++ run.sh
@@ -2,3 +2,4 @@
 python ./manage.py migrate
 python ./manage.py createsuperuser --noinput
 gunicorn -w 2 -b 0.0.0.0:8000 graduation_project.wsgi:application
+# python manage.py runserver 0.0.0.0:8000
